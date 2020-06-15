6ae7b2a4104c1a9f1b5dc0580f8b6b0469183327
Erik Butcher <butcer0@gmail.com>
2020-06-14 21:22:25 -0400

    Implemented Participant Registration Page
    Implemented Participant Registration Page Persisting Redux Form to State

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,36 @@
+import {DEFAULT_PARTICIPANT} from "../apis/participantsAPI";
 import {
     CREATE_PARTICIPANT,
     DELETE_PARTICIPANT,
     EDIT_PARTICIPANT,
     FETCH_PARTICIPANT,
     FETCH_PARTICIPANTS
-} from "../actions/types";
+} from "./types";
 
-// export const createParticipant = formValues => async (dispatch, getState) => {
-//
-// };
+let nextParticipantId = 0;
 
-// export const fetchParticipants = () => async dispatch => {
-//
-// };
+export const createParticipant = formValues => async (dispatch, getState) => {
+    formValues = {id: nextParticipantId, ...formValues};
+    nextParticipantId++;
+    const response = { data: formValues };
+    dispatch({ type: CREATE_PARTICIPANT, payload: response.data});
+};
 
-// export const fetchParticipant = id => async dispatch => {
-//
-// };
+export const fetchParticipants = () => async dispatch => {
+    const response = { data: DEFAULT_PARTICIPANT };
+    dispatch({ type: FETCH_PARTICIPANTS, payload: response.data })
+};
 
-// export const editParticipant = (id, formValues) => async dispatch => {
-//
-// };
+export const fetchParticipant = id => async dispatch => {
+    const response = { data: DEFAULT_PARTICIPANT }
+    dispatch({ type: FETCH_PARTICIPANT, payload: response.data })
+};
 
-// export const deleteParticipant = id => async dispatch => {
-//
-// };
+export const editParticipant = (id, formValues) => async dispatch => {
+    const response = { data: formValues };
+    dispatch({ type: EDIT_PARTICIPANT, payload: response.data});
+};
+
+export const deleteParticipant = id => async dispatch => {
+    dispatch({ type: DELETE_PARTICIPANT, payload: id });
+};
--- src/components/participants/ParticipantCreate.js
+++ src/components/participants/ParticipantCreate.js
@@ -1,16 +1,29 @@
 import * as React from "react";
+import {connect} from 'react-redux';
 
+import {createParticipant} from "../../actions";
 import ParticipantForm from "./ParticipantForm";
+import {ENVIRONMENTAL_EXPOSURES_LIST, GENE_MUTATIONS_LIST} from "../../apis/participantsAPI";
 
 class ParticipantCreate extends React.Component {
+    onSubmit = formValues => {
+        this.props.createParticipant(formValues);
+    };
+
     render() {
         return (
             <div>
                 <h3>Register a Participant</h3>
-                <ParticipantForm/>
+                <ParticipantForm
+                    envExposuresList={ENVIRONMENTAL_EXPOSURES_LIST}
+                    geneMutationsList={GENE_MUTATIONS_LIST}
+                    onSubmit={this.onSubmit}/>
             </div>
         )
     }
 }
 
-export default ParticipantCreate;
+export default connect(
+    null,
+    {createParticipant}
+)(ParticipantCreate);
